21ee99c385ccd9edaa4763434b455d2b22692062
felix <1306332027@qq.com>
2026-01-14 20:40:35 +0800

    初始化项目

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
+
+        // 兜底
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KnowledgeFlow"
+include(":app")
+ 
